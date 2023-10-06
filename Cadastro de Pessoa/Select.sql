f691cfcc07c9a143a00e035b489c214978a4eed9
francisco CGC <franciscotheslayer@hotmail.com>
2023-10-05 22:06:45 -0300

    select completo

--- Cadastro de Pessoa/Select.sql
+++ Cadastro de Pessoa/Select.sql
@@ -1,15 +1,201 @@
-create table city(
-id_c INTEGER constraint id_c_pk primary key, 
-dt_criacao DATE, 
-dt_halter DATE, 
-cd_city_ibge NUMBER(7) not null constraint cd_city_ibge_un unique,  
-dsc_city VARCHAR2(255));
+---Demonstração dos select, where, and, or e like--
 
-alter table city add cd_uf_ibge number(2);
-alter table city add constraint cd_uf_ibge_fk foreign key(cd_uf_ibge) references UF(cd_uf_ibge);
-
-delete city;
+SELECT * FROM Person_F;
+SELECT * FROM UF;
+SELECT * FROM city;
+SELECT * FROM endereco;
 
 select * 
 from city
-where cd_uf_ibge = '42'
+where cd_uf_ibge in(42,52)
+and (dsc_city like '%São%' 
+or dsc_city like '%Santa%' );   
+
+--Melhor forma de gerar relatório--
+
+select 
+    a.id_c "Sequencia",
+    a.cd_city_ibge" Codigo da cidade",
+    a.dsc_city"Cidade",
+    cd_uf_ibge"Codigo UF"
+from city a
+where a.cd_uf_ibge in(42,52,12)
+and (a.dsc_city like '%São%' 
+or a.dsc_city like '%Santa%' );  
+
+SELECT * FROM Person_F;
+SELECT * FROM UF;
+SELECT * FROM city;
+SELECT * FROM endereco;
+
+--Demonstração decode, case, to_char e to_numbe--
+
+select 
+    id_p,
+    nm,
+    dt_nascimento,
+    gener,
+    decode(gener,'M','Masculino','F','Feminino','Indeterminado')ds_gener
+from Person_f a;
+    
+select 
+    id_p,
+    nm,
+    dt_nascimento,
+    gener,
+    case when gener = 'M' then 'Masculino'
+        when gener = 'F' then 'Feminino'
+        else 'Indeterminado'
+        end ds_gener
+from Person_f a;
+    
+--r--
+    
+select 
+    id_p,
+    nm,
+    dt_nascimento,
+    gener,
+    (to_number(to_char(sysdate,'yyyy')))-(to_number(to_char(dt_nascimento,'YYYY')))qt_Idade,
+    case when(to_number(to_char(sysdate,'yyyy')))-(to_number(to_char(dt_nascimento,'YYYY')))between 1 and 60 then 'Jovem'
+         when(to_number(to_char(sysdate,'yyyy')))-(to_number(to_char(dt_nascimento,'YYYY')))between 61 and 999 then 'Idoso'
+    else 'Indeterminado' 
+    end Faixa_etaria
+from Person_f a;
+    
+    --comando nvl coalesce--   
+    
+select 
+    id_p,
+    nm,
+    dt_nascimento,
+    gener,
+    nvl(gener,'Indeterminado')ds_gener
+from Person_f a;
+
+select 
+    id_p,
+    nm,
+    dt_nascimento,
+    gener,
+    coalesce(gener,to_char(qt_tall), nm,'Indeterminado')ds_gener
+from Person_f a;
+    
+    
+    
+--demonstrando exists, not exists e join--
+
+select 
+    pf.id_p,
+    pf.nm,
+    pf.dt_nascimento,
+    pfe.nr_municipio,
+    c.dsc_city
+from Person_F pf
+left join endereco pfe on(pf.id_p = pfe.id_andress)
+left join city c on(c.id_c = pfe.nr_municipio);
+
+select 
+    pf.id_p,
+    pf.nm,
+    pf.dt_nascimento
+from Person_F pf
+where not exists (--caso queira mostrar as pessos com endereço usar o existis--
+    select 1
+    from endereco e
+    where e.nr_seq = pf.id_p);
+    
+--inner join--    
+select 
+    pf.id_p,
+    pf.nm,
+    pf.dt_nascimento,
+    c.dsc_city
+from Person_F pf
+join endereco pfe on(pf.id_p = pfe.id_andress)
+join city c on(c.id_c = pfe.nr_municipio)
+join uf on (uf.id_e = pfe.nr_uf );
+    
+
+--left join--
+
+select 
+    pf.id_p,
+    pf.nm,
+    pf.dt_nascimento,
+    c.dsc_city,
+    uf.dsc
+from Person_F pf
+left join endereco pfe on(pf.id_p = pfe.id_andress)
+left join city c on(c.id_c = pfe.nr_municipio)
+left join uf on (uf.id_e = pfe.nr_uf );
+
+
+--count, max,min, avg e sun--
+
+update person_f set person_f.qt_tall = trunc(dbms_random.value(1.2, 2.2),2);
+commit;
+
+
+select count(*)qt,
+    decode(gener,'M','Masculino','F','Feminino','Não Informado') gener
+from person_f
+group by decode(gener,'M','Masculino','F','Feminino','Não Informado');
+--
+
+select max(qt_tall)qt,
+    decode(gener,'M','Masculino','F','Feminino','Não Informado') gener
+from person_f
+group by decode(gener,'M','Masculino','F','Feminino','Não Informado');
+--
+
+select min(qt_tall)qt,
+            decode(gener,'M','Masculino','F','Feminino','Não Informado')gener
+from Person_F
+group by decode(gener,'M','Masculino','F','Feminino','Não Informado');
+--
+select sum(qt_tall)qt,
+            decode(gener,'M','Masculino','F','Feminino','Não Informado')gener
+from Person_F
+group by decode(gener,'M','Masculino','F','Feminino','Não Informado');
+--
+
+select trunc(avg(qt_tall),2)qt,
+            decode(gener,'M','Masculino','F','Feminino','Não Informado') gener
+from Person_F
+group by decode(gener,'M','Masculino','F','Feminino','Não Informado');
+
+
+
+--to_char, to_number e to_date--
+
+select  dt_nascimento,
+              to_char(dt_nascimento, 'dd')dia_nascimento,
+              to_char(dt_nascimento, 'd')dia_semana_nascimento,
+              to_char(dt_nascimento, 'day')dia_semana_extenso_nascimento,
+              to_number(to_char(dt_nascimento, 'mm'))mes_nascimento,
+              to_char(dt_nascimento, 'MON')mes_extenso_nascimento, 
+              to_char(dt_nascimento, 'yyyy')ano_nascimento
+from Person_F;
+--
+select  qt_tall,
+             trim(to_char(qt_tall,'90D99'))qt_tall2,
+             trim(to_char(0.9,'90D99')) qt_tall3
+from Person_F;
+--
+select n_cpf,
+             lpad(n_cpf,11,'0')n_cpf2,
+             rpad(n_cpf,11,'0')n_cpf3          
+from Person_F;
+
+
+
+
+
+
+
+
+
+
+
+commit;
---
+++ Cadastro de Pessoa/address.sql
@@ -0,0 +1,125 @@
+create sequence endereco_seq
+    start with 1
+    increment by 1
+    maxvalue 9999999
+    minvalue 1
+    nocache;
+    
+    
+    drop sequence endereco_seq;
+ 
+
+
+
+
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 164, 12, 1476);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 393, 30, 4176);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 477, 26, 2846);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 331, 5, 4712);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 335, 27, 2964);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 361, 27, 2371);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 457, 15, 1726);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 427, 27, 2646);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 139, 8, 3194);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 220, 26, 213);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 182, 21, 2001);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 178, 11, 4892);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 1, 15, 1633);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 358, 27, 820);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 333, 12, 5244);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 496, 16, 1931);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 264, 7, 168);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 393, 9, 3185);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 144, 20, 2469);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 419, 10, 4586);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 394, 12, 2736);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 435, 4, 1708);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 362, 21, 1732);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 413, 15, 4408);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 287, 19, 3948);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 126, 26, 2011);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 411, 25, 1541);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 302, 10, 209);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 305, 21, 1445);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 401, 9, 2718);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 351, 24, 4150);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 392, 19, 5063);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 227, 14, 5344);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 127, 25, 5053);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 70, 7, 3962);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 370, 5, 231);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 466, 7, 2273);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 360, 15, 2491);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 430, 14, 5382);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 375, 21, 1177);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 397, 15, 2558);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 29, 20, 131);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 350, 5, 5393);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 334, 17, 3690);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 135, 29, 5037);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 494, 13, 1450);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 40, 18, 4223);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 334, 18, 3539);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 468, 19, 1035);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 356, 18, 1437);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 61, 10, 4539);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 210, 12, 2373);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 465, 26, 706);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 340, 15, 3008);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 352, 9, 379);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 157, 22, 4365);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 354, 28, 5235);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 362, 15, 1084);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 423, 23, 5503);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 323, 24, 801);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 141, 25, 1529);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 392, 6, 4522);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 45, 9, 4445);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 16, 21, 4172);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 400, 24, 4139);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 413, 29, 1281);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 210, 21, 2794);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 64, 15, 5303);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 227, 29, 5444);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 402, 6, 419);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 285, 10, 3670);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 201, 16, 2750);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 177, 25, 2223);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 433, 13, 2089);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 267, 17, 4020);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 342, 18, 2213);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 213, 11, 2071);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 245, 24, 1129);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 49, 23, 4384);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 127, 19, 838);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 444, 21, 2285);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 229, 15, 724);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 122, 8, 3199);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 72, 22, 1799);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 303, 29, 836);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 112, 25, 5493);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 175, 27, 1292);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 175, 12, 4645);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 139, 21, 5264);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 223, 21, 270);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 322, 10, 2505);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 405, 12, 4318);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 253, 24, 3674);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 149, 25, 1352);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 360, 5, 1209);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 261, 23, 3224);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 95, 16, 3398);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 10, 20, 4890);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 492, 8, 1799);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 178, 27, 474);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 90, 27, 2177);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 13, 29, 83);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 50, 7, 4048);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 405, 17, 3527);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 115, 6, 5044);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 225, 27, 27);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 207, 16, 615);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 278, 16, 1415);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 463, 20, 2246);
+insert into endereco(id_andress, dt_criacao,nr_seq,  nr_uf,  nr_municipio) values(endereco_seq.nextval, sysdate, 387, 8, 1256);
+commit;
